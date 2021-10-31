34ddae6a64636220393a36123449c025de54abf9
Adnan Valdes <adravaldes+github@gmail.com>
2021-10-30 18:29:17 -0700

    Use pandas. Does not account for two equal STRs for the same person

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -1,6 +1,5 @@
 from sys import argv
-import csv
-
+import pandas as pd
 
 def main():
     
@@ -8,12 +7,26 @@ def main():
         print("Usage: python dna.py data.csv sequence.txt")
         exit(1)
 
-    countSTR('AA', './sequences/1.txt')
+    df = pd.read_csv(argv[1])
+    STRs = df.loc[:, df.columns != 'name'].columns
 
+    for STR in STRs:
+        for i in range(len(df)):
 
+            try:
 
+                if df.iloc[i][STR] != countSTR(STR, argv[2]):
+                    # Check that there is only one max value
+                    df = df[df.name != df.iloc[i]['name']]
+            
+            except IndexError:
+                break
 
+    if len(df) == 1:
+        print(df['name'].values[0])
+        exit(0)
 
+    print("No match")
 
 def countSTR(STR, sequence):
     str_count = 0
@@ -22,15 +35,12 @@ def countSTR(STR, sequence):
 
     with open(sequence, "r") as seq:
         seq = seq.read()
-        seq = 'AABBCCAAAAAA'
         seq_len = len(seq)
        
         i = 0 
         while i < seq_len:
             current = seq[i:i+str_len]
-            print(current)
             if current == STR:
-                print("MATCH!")
                 str_count += 1
                 i += str_len
 
@@ -41,8 +51,10 @@ def countSTR(STR, sequence):
                     str_count_arr.append(str_count)
                 i += 1
                 str_count = 0
-        print(max(str_count_arr))
-        return max(str_count_arr)               
+       
+        if len(str_count_arr) == 0:
+            return 0
+        return max(str_count_arr)              
    
 if __name__ == "__main__":
     main()
