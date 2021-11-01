815998412525a7ed6789c2fd8080afd06dfff131
Adnan Valdes <adravaldes+github@gmail.com>
2021-10-31 17:47:51 -0700

    Add limit 5 to 11.sql

--- pset7/movies/11.sql
+++ pset7/movies/11.sql
@@ -1,1 +1,6 @@
-select title from movies join stars on movies.id = stars.movie_id join people on stars.person_id = people.id join ratings on movies.id=ratings.movie_id where people.name = "Chadwick Boseman" order by ratings.rating desc;
+select title from movies 
+join stars on movies.id = stars.movie_id 
+join people on stars.person_id = people.id 
+join ratings on movies.id=ratings.movie_id 
+where people.name = "Chadwick Boseman" 
+order by ratings.rating desc limit 5;
