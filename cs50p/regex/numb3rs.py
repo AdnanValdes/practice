ded4766b61f92f010bffa624b341276095b39627
einhard <adnan.valdes@terminus.earth>
2024-07-29 16:19:02 -0700

    numb3rs.py

--- cs50p/regex/numb3rs.py
+++ cs50p/regex/numb3rs.py
@@ -1,10 +1,14 @@
 import re
 import sys
 
-IPV4 = r"^(25[0-5]?|2[0-4]\d|1\d\d|\d\d|\d)(\.(25[0-5]?|2[0-4]\d|1\d\d|\d\d|\d)){2}\.(25[0-5]?|2[0-4]\d|1\d\d|\d\d|\d)$"
+IPV4 = (r"^(25[0-5]?|2[0-4]\d|1\d\d|\d\d|\d)" # Match first octet
+        r"(\.(25[0-5]?|2[0-4]\d|1\d\d|\d\d|\d)){2}" # Match middle octet twice with leading period (.)
+        r"\.(25[0-5]?|2[0-4]\d|1\d\d|\d\d|\d)$") # Match last octet
+
 
 def main():
     print(validate(input("IPv4 Address: ")))
+    sys.exit(0)
 
 def validate(ipv4: str) -> bool:
     if re.search(IPV4, ipv4):
--- cs50p/regex/test_numb3rs.py
+++ cs50p/regex/test_numb3rs.py
@@ -1,6 +1,7 @@
 import pytest
 from numb3rs import validate
 
+
 def test_correct_validate():
     assert validate("1.1.1.1") == True
     assert validate("127.0.0.1") == True
@@ -10,8 +11,10 @@ def test_correct_validate():
     assert validate("64.78.200.1") == True
     assert validate("0.0.0.0") == True
 
+
 def test_wrong_validate():
     assert validate("256.0.0.0") == False
     assert validate("1111.1.1.1") == False
     assert validate("300.1.1.1") == False
     assert validate("cat") == False
+    assert validate("123.125.123.643") == False
