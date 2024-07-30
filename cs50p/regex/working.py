37d3ce33c61c9b529b7d2723f779b478b017db8b
einhard <adnan.valdes@terminus.earth>
2024-07-29 17:38:17 -0700

    working.py

---
+++ cs50p/regex/working.py
@@ -0,0 +1,38 @@
+import re
+import sys
+
+FORMAT = (
+    r"(?P<shrs>1{0,1}\d):?(?P<smin>[0-5]\d)? (?P<smer>AM|PM)"
+    r" to "
+    r"(?P<ehrs>1{0,1}\d):?(?P<emin>[0-5]\d)? (?P<emer>AM|PM)"
+)
+
+
+def main():
+    print(convert(input("Hours: ")))
+
+
+def convert(s):
+    if m := re.search(FORMAT, s):
+        start = manage(m.group("shrs"), m.group("smin"), m.group("smer"))
+        end = manage(m.group("ehrs"), m.group("emin"), m.group("emer"))
+
+        return f"{start} to {end}"
+    else:
+        raise ValueError
+
+
+def manage(hrs, mins, meridian):
+    if mins == None:
+        mins = 00
+    hrs, mins = int(hrs), int(mins)
+    if hrs > 12 or mins >= 60:
+        raise ValueError
+    if meridian == "AM":
+        return f"{hrs:02}:{mins:02}"
+    else:
+        return f"{hrs+12:02}:{mins:02}"
+
+
+if __name__ == "__main__":
+    main()
